51a83c7b8f8cd34cab7918048471f3b1977c505d
dlsgur2845 <dlsgur2845@naver.com>
2022-09-13 01:00:36 +0900

    컬렉션 조회 최적화

---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,110 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    /**
+     * v1 처럼 qpi entity를 직접 노출하면 안됨.
+     */
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+
+    /**
+     * v2 역시 entity가 노출됨.
+     * entity가 노출되면 안됨. dto로 감싼다고해서 노출이 완벽히 차단되는 것은 아님.
+     * orderItems가 모두 노출됨. 즉, orderItems도 모두 dto로 변경해야함.
+     *
+     * v2는 쿼리가 상당히 많이 실행됨... 성능 저하의 원인
+     */
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    /**
+     * ManyToMany 관계에서는 데이터 join시 뻥튀기 조심 (hibernate에서 체크 못함)
+     * jpa에서 select 시 distinct 추가 (sql의 distinct와는 결과물이 약간 다름)
+     *
+     * 페이징 쿼리 썻을 때 jpa는 모든 데이터를 들고온 후 메모리에서 페이징
+     *   - 예상 성능이 안나올 뿐만 아니라, Out Of Mem을 초래할 수 있음.
+     *      -> 1대 다에서 페이징을 사용하지 않을때만 fetch join 사용.
+     */
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Getter
+    static class OrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    static class OrderItemDto {
+
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
+}
